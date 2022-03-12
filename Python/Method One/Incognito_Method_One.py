5e8747f94a3d1bbb46af9d0e844ead6c81a3478e
Yasmin Beaumont <96609891+VanellopeReal@users.noreply.github.com>
2022-03-11 23:17:05 -0300

    Add files via upload

---
+++ Python/Method One/Incognito_Method_One.py
@@ -0,0 +1,5 @@
+from subprocess import call
+call(r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe -inprivate")
+#Edge uses -inprivate, Chrome -incognito.
+#You should be searching or testing which (or what) your browser is using
+#In Parameter names
